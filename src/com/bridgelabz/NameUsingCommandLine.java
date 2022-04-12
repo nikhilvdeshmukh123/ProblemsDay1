b7740c15173c9cf4b3c00bc4832cbbcd64e3417b
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-12 07:37:41 +0530

    Added: Get the name by command line

---
+++ src/com/bridgelabz/NameUsingCommandLine.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/*
+ * @author: Nikhil
+ * Get the Name by using of command line
+ */
+public class NameUsingCommandLine {
+	public static void main(String[] args) {	
+		String[] fruits = {"Apple","Orange"};
+		System.out.println("Argument one: " + fruits[0]);
+		System.out.println("Argument Two: " + fruits[1]);
+	}
+}
